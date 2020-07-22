c151e2a6925e267ec3aea71fe116bc1de03b767f
tharaka <stwijasinghe@gmail.com>
2020-07-22 11:33:36 +0530

    initial commit

---
+++ Student.java
@@ -0,0 +1,18 @@
+public Student{
+    int id;
+    String name;
+
+    public Student(){
+
+    }
+
+    public Student(int id,String name){
+        this.id = id;
+        this.name=name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+}
